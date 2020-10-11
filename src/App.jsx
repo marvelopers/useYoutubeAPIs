66526dafc54d7c1d9c6098c20791862d41252b2e
marvelopers <marvelopers@gmail.com>
2020-10-11 13:07:42 +0900

    Add basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import styles from './app.module.css';
+import VideoList from './components/video_list/video_list';
+import SearchHeader from './components/search_header/search_header';
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
-  const [name, setName] = useState('Chloe');
+  const search = query => {
+    youtube
+      .search(query) //
+      .then(videos => setVideos(videos));
+  };
 
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    }
-
-    fetch(
-      'https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAtrSZkopq--QXlpEYQ5SrM9Kg5TZlZMl0',
-      requestOptions
-    )
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error))
+    youtube
+      .mostPopular() //
+      .then(videos => setVideos(videos));
   }, []);
-
   return (
-    <VideoList videos={videos} />
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
+      <VideoList videos={videos} />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './app';
+import Youtube from './service/youtube';
 
+const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App youtube={youtube} />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
