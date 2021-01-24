4e7c14de876d370191653157090146e9fa73f871
marvelopers <marvelopers@gmail.com>
2021-01-24 23:54:24 +0900

    Add youtube API fetch 코드 추가

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # useYoutubeAPIs
+
 youtube APIs를 사용한 React Project
 
-## add 
+## add
 
 ### REST API
+
 - GET, POST, PUT, DELETE
 
+[apiLink](https://developers.google.com/youtube/v3/getting-started?hl=ko)
--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,24 @@ import styles from './app.module.css';
 import VideoList from './components/video_list/video_list';
 import SearchHeader from './components/search_header/search_header';
 
-function App({ youtube }) {
+function App() {
   const [videos, setVideos] = useState([]);
-  const search = query => {
-    youtube
-      .search(query) //
-      .then(videos => setVideos(videos));
-  };
 
   useEffect(() => {
-    youtube
-      .mostPopular() //
-      .then(videos => setVideos(videos));
-  }, );
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      'https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAtrSZkopq--QXlpEYQ5SrM9Kg5TZlZMl0',
+      requestOptions
+    )
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+
 
   return (
     <div className={styles.app}>
